cca1209aca450a8e6dee86d014889503d32f8e40
Masum-Reza <careerhunter4280@gmail.com>
2023-07-15 09:54:34 -0700

    folder renamed

--- Lecture-45_className/classList/index.js
+++
@@ -1,9 +0,0 @@
-let container = document.getElementById('container');
-//here i remember the attribute methods most similer
-// container.className = 'yellow dark red tshirt tpant andSOon'; //it will set the classes into the container
-// container.classList;//it will show all classes of the element
-// container.classList.remove('dark'); //it will particularly remove the called class
- 
-// container.classList.add('mr-addingson'); //to add a new class
-// container.classList.contains('mr-addingson') //to check if the class available
-// container.classList.toggle('red') //if class red available remove else add
